7dcfad8a510bba641006d47424a4b95f91f3b13c
Deniz <77787415+deniz244@users.noreply.github.com>
2023-08-24 23:55:29 +0300

    index.css kullanmadığı için kaldırdım

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import './index.css';
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 import "./reset.css";
 import DarkModeTheme from "./contexts/DarkModeContext";
@@ -16,8 +14,3 @@ root.render(
     </DarkModeTheme>
   </Language>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
