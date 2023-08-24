06be7f3aa27952c3fe01560939e3377ac9ea7b72
Deniz <77787415+deniz244@users.noreply.github.com>
2023-08-24 23:10:02 +0300

    projectsData yapıldı, proje imgler eklendi

---
+++ src/Data/projectsData.js
@@ -0,0 +1,70 @@
+const data = [
+  {
+    id: 1,
+    language: "tr-TR",
+    title: "Hesap makinesi",
+    description:
+      "Sayıları herhangi bir sırayla toplayabilen, çarpabilen, bölebilen ve çıkartabilen, ayrıca hafızaya kaydetme ve callback özellikleri ekleyebilen bir hesap makinesi",
+    websiteLink: "https://fsweb-s10g1-reducer-calculator-eta.vercel.app/",
+    image: "hesapM.png",
+    yazilimlari: ["#JS", "#React", "#Axios", "#Router", "#Cypress", "#Yup"],
+    githubLink: "https://github.com/deniz244/fsweb-s10g1-reducer-calculator",
+  },
+  {
+    id: 2,
+    language: "en-US",
+    title: "Reducer Calculator",
+    description:
+      "A calculator that can add, divide, subtract and subtract numbers in any order, as well as add storage and callback features to memory",
+    websiteLink: "https://fsweb-s10g1-reducer-calculator-eta.vercel.app/",
+    image: "hesapM.png",
+    yazilimlari: ["#JS", "#React", "#Axios", "#Router", "#Cypress", "#Yup"],
+    githubLink: "https://github.com/deniz244/fsweb-s10g1-reducer-calculator",
+  },
+  {
+    id: 3,
+    language: "tr-TR",
+    title: "Alışveriş Sepeti",
+    description:
+      "Alışveriş Sepeti Uygulaması, React ve React Router ile geliştirilen dinamik bir web uygulaması. Ürünlere göz atın ve sepete ekleyerek sorunsuz bir alışveriş deneyimi yaşayın.",
+    websiteLink: "https://fsweb-s11g1-shopping-cart-xi.vercel.app/",
+    image: "sepet.png",
+    yazilimlari: ["#JS", "#React", "#Axios", "#Router", "#Cypress"],
+    githubLink: "https://github.com/deniz244/fsweb-s11g1-shopping-cart",
+  },
+  {
+    id: 4,
+    language: "en-US",
+    title: "Shopping Cart",
+    description:
+      "Introducing my E-commerce App project, a dynamic web application built with React and React Router. Browse products and add to cart for a seamless shopping experience.",
+    websiteLink: "https://fsweb-s11g1-shopping-cart-xi.vercel.app/",
+    image: "sepet.png",
+    yazilimlari: ["#JS", "#React", "#Axios", "#Router", "#Cypress"],
+    githubLink: "https://github.com/deniz244/fsweb-s11g1-shopping-cart",
+  },
+  {
+    id: 5,
+    language: "tr-TR",
+    title: "Film Listesi",
+    description:
+      "Film listeleyebileceğiniz bir uygulama ve filmleri favorilerinize ekleyebilirsiniz. Bu sayede favori filmlerinizi daha sonra izleyebilirsiniz.",
+    websiteLink: "https://fsweb-s10g3-redux-watchlist-gray.vercel.app/",
+    image: "film.png",
+    yazilimlari: ["#JS", "#React", "#Axios", "#Router", "#Cypress"],
+    githubLink: "https://github.com/deniz244/fsweb-s10g3-redux-watchlist",
+  },
+  {
+    id: 6,
+    language: "en-US",
+    title: "Movie List",
+    description:
+      "A movie listing application where you can add movies to your favorites. This way, you can watch your favorite movies later.",
+    websiteLink: "https://fsweb-s10g3-redux-watchlist-gray.vercel.app/",
+    image: "film.png",
+    yazilimlari: ["#JS", "#React", "#Axios", "#Router", "#Cypress"],
+    githubLink: "https://github.com/deniz244/fsweb-s10g3-redux-watchlist",
+  },
+];
+
+export default data;
